dae8cf8d194498ec3f672c4b00754d5120972eba
JanetteDevCode <30553904+JanetteDevCode@users.noreply.github.com>
2019-11-10 13:00:39 -0800

    Complete course contents step 9

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -33,7 +33,7 @@ const Content = ({ parts }) => {
 const Total = ({ exercises }) => {
   const sumOfExercises = () => {
     return exercises.reduce((accumulator, currentValue) => {
-      return accumulator + currentValue
+      return accumulator + currentValue;
     });
   };
   return (
@@ -46,7 +46,7 @@ const Total = ({ exercises }) => {
 const Course = ({ course }) => {
   const exercises = () => {
     return course.parts.map((part) => {
-      return part.exercises
+      return part.exercises;
     });
   };
   return (
@@ -59,29 +59,60 @@ const Course = ({ course }) => {
 };
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
+  const showCourses = () => {
+    return courses.map((course) => {
+      return (
+        <Course key={course.id} course={course} />
+      );
+    });
   };
   return (
     <div>
-      <Course course={course} />
+      {showCourses()}
     </div>
   );
 };
